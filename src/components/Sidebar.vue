ab11864b0a951f37281b96e406c681c6fb3d4c1f
Hosana <hosanabarceloscandido@gmail.com>
2024-06-19 15:10:40 -0300

    feat: inicia componente de sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+	<header>
+		<h1 class="logo">
+			<img src="../assets/logo.png" alt="Logo">
+			<p>Tracker</p>
+		</h1>
+	</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: 'Sidebar'
+});
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #80afecb0;
+  width: 100%;
+  height: 100vh;
+}
+header img {
+	opacity: .6;
+	width: 4rem;
+}
+.logo {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 15px;
+}
+p {
+	font-size: 2rem;
+	font-weight: 600;
+	color: #FFFFFF;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+	padding: 2.5rem;
+	height: auto;
+  }
+  header img {
+	width: 2rem;
+  }
+}
+</style>
