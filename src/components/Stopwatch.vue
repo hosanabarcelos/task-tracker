95a9e64feaedb5f20e0d06139eed6d192600f841
Hosana <hosanabarceloscandido@gmail.com>
2024-06-19 16:29:37 -0300

    refactor: cria componente stopwatch e timer

---
+++ src/components/Stopwatch.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <strong>{{ printTime }}</strong>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Stopwatch',
+
+    props: {
+        time: {
+            type: Number,
+            default: 0
+        }
+    },
+    computed: {
+    printTime() : string {
+        return new Date(this.time * 1000).toISOString().substr(11,8);
+    }
+  },
+});
+</script>
---
+++ src/components/Timer.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    class="column is-flex is-align-items-center is-justify-content-space-between"
+  >
+    <Stopwatch :time="time" />
+    <div class="buttons">
+      <button class="button" @click="start">
+        <span class="icon">
+          <i class="fas fa-play"></i>
+        </span>
+      </button>
+      <button class="button" @click="stop">
+        <span class="icon">
+          <i class="fas fa-stop"></i>
+        </span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Stopwatch from "./Stopwatch.vue";
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Form",
+  data() {
+    return {
+        time: 0,
+        stopwatch: 0,
+    }
+  },
+  components: {
+    Stopwatch
+  },
+  methods: {
+    start() {
+        this.stopwatch = setInterval(() => {
+            this.time += 1;
+        }, 1000);
+    },
+    stop() {
+        clearInterval(this.stopwatch);
+    }
+  }
+});
+</script>
+
+<style scoped>
+.buttons {
+  display: flex;
+  gap: 10px;
+}
+
+.box {
+  width: 100%;
+}
+
+.column {
+  display: flex;
+  align-items: center;
+}
+
+.is-flex.is-align-items-center.is-justify-content-space-between {
+  justify-content: space-between;
+}
+</style>
