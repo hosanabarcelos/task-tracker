98f58b66e3cc61e3a1c409751c889d6ab3a7a88a
Hosana <hosanabarceloscandido@gmail.com>
2024-06-20 18:09:47 -0300

    feat: importa componente de tarefa e adiciona lógica para o formulário

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,39 @@
             <Sidebar />
         </div>
         <div class="column is-three-quarter">
-            <Form />
+            <Form @submitTask="saveTask"/>
             <div class="list">
-                <Task />
-                <Task />
-                <Task />
-                <Task />
+                <Task
+                    :key="index"
+                    v-for="(task, index) in tasks"
+                    :task="task"
+                />
             </div>
         </div>
     </main>
 </template>
 
-<script>
+<script lang="ts">
 import Sidebar from './components/Sidebar.vue';
 import Form from './components/Form.vue';
 import Task from './components/Task.vue';
+import Tasks from './interfaces/Task';
 
 export default {
+    data() {
+        return {
+            tasks: [] as Tasks[]
+        }
+    },
     components: {
         Sidebar,
         Form,
         Task
+    },
+    methods: {
+        saveTask( task: Tasks) {
+            this.tasks.push(task)
+        }
     }
 }
 </script>
@@ -37,6 +49,6 @@ body {
     width: 20% !important;
 }
 .list {
-    padding: 1.25rem;
+    padding: 1rem;
 }
 </style>
