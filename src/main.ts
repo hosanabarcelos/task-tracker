b3f0e9677ec72fc3a0c14fa9bebfc75df602ec2d
Hosana <hosanabarceloscandido@gmail.com>
2024-06-20 13:53:01 -0300

    feat: importa arquivo global de estilo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import '@/assets/styles/global.css';
 
 import '@fortawesome/fontawesome-free/css/all.css';
 
