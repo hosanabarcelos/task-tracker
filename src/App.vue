434b0fc43008357537b12d463a8c51c50c79fd99
Hosana <hosanabarceloscandido@gmail.com>
2024-06-20 18:33:26 -0300

    feat: importa componente box e add empty-state

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,13 @@
                     v-for="(task, index) in tasks"
                     :task="task"
                 />
+                <Box v-if="emptyList">
+                    <div class="empty-state">
+                        <img src="./assets/logo.png" alt="Logo Task Tracker">
+                        <p>Opa!<br>
+                        Parece que você ainda não começou nenhuma tarefa. Vamos começar?</p>
+                    </div>
+                </Box>
             </div>
         </div>
     </main>
@@ -21,6 +28,7 @@ import Sidebar from './components/Sidebar.vue';
 import Form from './components/Form.vue';
 import Task from './components/Task.vue';
 import Tasks from './interfaces/Task';
+import Box from './components/Box.vue'
 
 export default {
     data() {
@@ -31,7 +39,13 @@ export default {
     components: {
         Sidebar,
         Form,
-        Task
+        Task,
+        Box
+    },
+    computed: {
+        emptyList() {
+            return this.tasks.length === 0;
+        }
     },
     methods: {
         saveTask( task: Tasks) {
@@ -50,5 +64,21 @@ body {
 }
 .list {
     padding: 1rem;
+}
+.empty-state {
+    height: 75vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+    gap: 18px;
+}
+.empty-state img {
+    width: 8rem;
+}
+.empty-state p {
+    color: #797979;
+    font-size: 1.2rem;
 }
 </style>
