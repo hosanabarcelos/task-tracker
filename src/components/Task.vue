b984dd866a10ceaafc77d83f72e7ab83125cffbf
Hosana <hosanabarceloscandido@gmail.com>
2024-06-20 13:53:33 -0300

    feat: inicia componente de tarefa

---
+++ src/components/Task.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-7">
+                Descrição da tarefa
+            </div>
+            <div class="column">
+                <Stopwatch
+                    time="15"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import Stopwatch from './Stopwatch.vue';
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Task',
+
+    components: {
+        Stopwatch,
+    }
+});
+</script>
