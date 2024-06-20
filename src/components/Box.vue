a7d09dd5fe288f32a4e74c9f9d36c353fb612578
Hosana <hosanabarceloscandido@gmail.com>
2024-06-20 18:33:38 -0300

    feat: cria componente box

---
+++ src/components/Box.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="box">
+        <slot />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Box',
+});
+</script>
